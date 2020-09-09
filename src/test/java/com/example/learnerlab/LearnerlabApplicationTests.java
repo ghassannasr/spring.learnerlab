a0f146b11a7c973b1d820c697c3e6bfeda852bde
Ghassan Nasr <ghassan.nasr@gmail.com>
2020-09-08 20:58:33 -0400

    extracted contents of project generated by start.spring.io

---
+++ src/test/java/com/example/learnerlab/LearnerlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnerlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnerlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
